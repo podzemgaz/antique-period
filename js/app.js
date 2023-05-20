8929735b13b75b405163ec697d584b5d0cf5d7a4
podzemgaz <podzemgaz@gmail.com>
2023-05-20 22:20:53 +0300

    first page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
+loadCSS('main-page.css');
 init();
 const toHeroes = document.getElementsByClassName('to-heroes');
 const heroes = document.getElementById('heroes');
 const toMain = document.getElementsByClassName('to-main-page');
-const content = document.getElementById('content');
+const content = document.querySelector('main');
 const book = document.getElementById('book');
 
 
 
 
-book.addEventListener('click', setActiveBook);
+book.addEventListener('click', toBookPage);
 
 
 
@@ -23,14 +24,14 @@ for (let index = 0; index < toHeroes.length; index++) {
 }
 
 function init() {
+    
     $.get(getActivePage(), function (data) {
-
         content.innerHTML = data;
         let startReading = document.querySelector('#start-reading');
         if (startReading) {
-            startReading.addEventListener('click', setActiveBook);
+            startReading.addEventListener('click', toBookPage);
         }
-        addActive();
+        colorActiveNavButton();
 
     });
 
@@ -42,16 +43,19 @@ function getActivePage() {
     return page ? page : "main-page.html";
 }
 
+function getActiveCss() {
+    return getActivePage().replace('.html', '.css');
+}
+
 function getActiveElement() {
 
     const activePage = getActivePage();
 
-    console.log(activePage);
     const id = activePage.replace('.html', '');
     return id === "main-page" ? "" : document.getElementById(id);
 }
 
-function addActive() {
+function colorActiveNavButton() {
     const activeElement = getActiveElement();
     if (activeElement) {
         // console.log(true);
@@ -59,7 +63,7 @@ function addActive() {
     }
 }
 
-function removeActive() {
+function removeColorActiveNavButton() {
     const activeElement = getActiveElement();
     if (activeElement) {
         // console.log(activeElement);
@@ -68,24 +72,49 @@ function removeActive() {
 }
 
 function toHeroesPage() {
-    removeActive();
-    localStorage.setItem('ACTIVE_PAGE', 'heroes.html');
-
-    heroes.classList.add('active');
-    init();
+    setActivePage('heroes.html');
 }
 
-function setActiveBook() {
-    removeActive();
-    localStorage.setItem('ACTIVE_PAGE', 'book.html');
-
-    book.classList.add('active');
-    init();
+function toBookPage() {
+    setActivePage('book.html');
 }
 
 function toMainPage() {
-    removeActive();
-    localStorage.setItem('ACTIVE_PAGE', '');
+    setActivePage('main-page.html');
+}
+
+function setActivePage(page) {
+    removeColorActiveNavButton();
+    reloadCss(page);
+    localStorage.setItem('ACTIVE_PAGE', page);
     init();
 }
 
+function reloadCss(fileName) {
+    var css = fileName.replace(".html", '.css');
+    var activeCss = getActiveCss();
+    
+    if (activeCss !== css) {
+        unloadCSS(activeCss);
+        loadCSS(css);
+    } 
+}
+
+function loadCSS(filename) {
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('type', 'text/css');
+    link.setAttribute('href', 'css/' + filename);
+    document.head.appendChild(link);
+}
+
+function unloadCSS(filename) {
+    var links = document.getElementsByTagName('link');
+    for (var i = 0; i < links.length; i++) {
+      var link = links[i];
+      if (link.getAttribute('href') === 'css/' + filename) {
+        link.parentNode.removeChild(link);
+        break;
+      }
+    }
+  }
